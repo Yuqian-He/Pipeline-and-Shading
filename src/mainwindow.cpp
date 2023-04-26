3c3f9bce9182e1c7bf950db3ec007885b4df4375
Yuqian-He <ashley_525@outlook.com>
2023-04-26 19:23:16 +0100

    finished qt gui

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "../include/mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
+#include<QFileDialog>
+#include<QMessageBox>
+#include<QDesktopServices>
+#include<QUrl>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +15,63 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filename=QFileDialog::getOpenFileName(this,tr("Open File"),"", "Obj Files (*.obj*)");
+    if (!filename.isEmpty()) {
+        ui->lineEdit->setText(filename);
+    }
+    modelPath=filename;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString filename=QFileDialog::getOpenFileName(this,tr("Open File"),"", "Jpg Files (*.jpg*)");
+    if (!filename.isEmpty()) {
+        ui->lineEdit_2->setText(filename);
+    }
+    normalPath=filename;
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString filename=QFileDialog::getOpenFileName(this,tr("Open File"),"", "Jpg Files (*.jpg*)");
+    if (!filename.isEmpty()) {
+        ui->lineEdit_3->setText(filename);
+    }
+    texturePath=filename;
+}
+
+
+void MainWindow::on_texture_clicked()
+{
+    QPixmap pix("/Users/naname/Documents/GAMES101/Assignment3/Pipeline-and-Shading/build/output.png");
+    ui->image->setPixmap(pix);
+}
+
+
+void MainWindow::on_normal_clicked()
+{
+    QPixmap pix("/Users/naname/Documents/GAMES101/Assignment3/Pipeline-and-Shading/build/output.png");
+    ui->image->setPixmap(pix);
+}
+
+
+void MainWindow::on_blinnPhong_clicked()
+{
+    QPixmap pix("/Users/naname/Documents/GAMES101/Assignment3/Pipeline-and-Shading/build/output.png");
+    ui->image->setPixmap(pix);
+}
+
+
+void MainWindow::on_noBlinnPhong_clicked()
+{
+    QPixmap pix("/Users/naname/Documents/GAMES101/Assignment3/Pipeline-and-Shading/build/output.png");
+    ui->image->setPixmap(pix);
 }
 
