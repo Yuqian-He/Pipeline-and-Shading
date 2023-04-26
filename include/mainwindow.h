27990a73d36edfeb7f989499d70bf1891ce22151
Yuqian-He <ashley_525@outlook.com>
2023-04-26 19:23:29 +0100

    finished qt gui

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -12,8 +12,26 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QString modelPath;
+    QString texturePath;
+    QString normalPath;
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_texture_clicked();
+
+    void on_normal_clicked();
+
+    void on_blinnPhong_clicked();
+
+    void on_noBlinnPhong_clicked();
 
 private:
     Ui::MainWindow *ui;
